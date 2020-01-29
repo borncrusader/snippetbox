f2e81bdf1a37aead88c28905d3f1e26636e3124d
Srinath Krishna Ananthakrishnan <srinath.krishna@gmail.com>
2020-01-28 16:54:48 -0800

    Adding custom template functions

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,14 +1,25 @@
 package main
 
-import "srinathkrishna.in/snippetbox/pkg/models"
+import (
+	"html/template"
+	"path/filepath"
+	"time"
 
-import "html/template"
-
-import "path/filepath"
+	"srinathkrishna.in/snippetbox/pkg/models"
+)
 
 type templateData struct {
-	Snippet  *models.Snippet
-	Snippets []*models.Snippet
+	CurrentYear int
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
@@ -22,7 +33,7 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
